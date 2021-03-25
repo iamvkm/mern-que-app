c800cd04536f5bdedd1fd9d10a687d888896cc9c
Vivek Mishra <vivekhere01@gmail.com>
2021-03-25 13:12:23 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sheetData from "./questions.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App container">
+        {sheetData.map((item) => (
+          <div>
+            <p className="que" key={item.queId}>
+              {item.question}
+            </p>
+            <ol>
+              <li>{item.optOne}</li>
+              <li>{item.optTwo}</li>
+              {!item.optThree ? "" : <li>{item.optThree}</li>}
+              {!item.optFour ? "" : <li>{item.optFour}</li>}
+            </ol>
+            <p className="ans">
+              <span>Correct answer: </span>
+              {item.answer}
+            </p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
