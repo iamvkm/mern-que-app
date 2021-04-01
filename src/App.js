5d3bcec4d2dfdbfc051746f3aeba426b3b2b1dc0
Vivek Mishra <vivekhere01@gmail.com>
2021-04-01 21:17:58 +0530

    updated links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import "./App.css";
-import TestThree from "./TestThree";
-import TestFour from "./TestFour";
+import sheetData from "./data/testThree.json";
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
+
+function TestThree() {
+  return (
+    <div className="App container">
+      <p>
+        Go back <Link to="/">Home</Link>
+      </p>
+      {sheetData.map((item) => (
+        <div>
+          <p className="que" key={item.queId}>
+            {item.question}
+          </p>
+          <ol>
+            <li>{item.optOne}</li>
+            <li>{item.optTwo}</li>
+            {!item.optThree ? "" : <li>{item.optThree}</li>}
+            {!item.optFour ? "" : <li>{item.optFour}</li>}
+          </ol>
+          <p className="ans">
+            <span>Correct answer: </span>
+            {item.answer}
+          </p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function TestFour() {
+  return (
+    <div className="App container">
+      <h3>Test Four - Coming Soon!</h3>
+      <p>
+        To access test three <Link to="/three">click here</Link>
+      </p>
+    </div>
+  );
+}
 
 function App() {
   return (
