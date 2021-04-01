10a04ab379ab168a5ae4ea280e16b103b0881384
Vivek Mishra <vivekhere01@gmail.com>
2021-04-01 21:54:31 +0530

    updated links

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import "./App.css";
-import sheetData from "./data/testThree.json";
 
-import { Switch, Route, Link } from "react-router-dom";
+import TestThree from "./components/TestThree";
+import TestFour from "./components/TestFour";
 
-function TestThree() {
-  return (
-    <div className="App container">
-      <p>
-        Go back <Link to="/">Home</Link>
-      </p>
-      {sheetData.map((item) => (
-        <div>
-          <p className="que" key={item.queId}>
-            {item.question}
-          </p>
-          <ol>
-            <li>{item.optOne}</li>
-            <li>{item.optTwo}</li>
-            {!item.optThree ? "" : <li>{item.optThree}</li>}
-            {!item.optFour ? "" : <li>{item.optFour}</li>}
-          </ol>
-          <p className="ans">
-            <span>Correct answer: </span>
-            {item.answer}
-          </p>
-          <hr />
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function TestFour() {
-  return (
-    <div className="App container">
-      <h3>Test Four - Coming Soon!</h3>
-      <p>
-        To access test three <Link to="/three">click here</Link>
-      </p>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/components/TestFour.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function TestFour() {
+  return (
+    <div className="App container">
+      <h3>Test Four - Coming Soon!</h3>
+      <p>
+        To access test three <Link to="/three">click here</Link>
+      </p>
+    </div>
+  );
+}
+
+export default TestFour;
